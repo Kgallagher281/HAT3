7dd6839f866cff000971653346e5fabc93d75867
kgallagher <kevin.gallagher23@gmail.com>
2017-11-09 06:13:13 -0800

    final mapping done

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20171108155742) do
     t.string "name"
     t.string "address"
     t.float "longitude"
-    t.float "lattitude"
+    t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
