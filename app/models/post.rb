5bad776c2abe75f2afea5be494c2cb780fa2f37b
Boony Guzman <josueguzman89@gmail.com>
2017-11-10 10:12:43 -0500

    posts/showpost

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  has_attached_file :photo, styles: { medium: "200x200>", thumb: "50x50>", large: '300x300>'}, default_url: "/missing.png"
+  has_attached_file :photo, styles: { medium: "200x200>", thumb: "50x50>", large: '596.66x596.66>'}, default_url: "/missing.png"
 	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
