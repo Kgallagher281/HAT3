9b3bd858a939a0f6c47ea65726f66923d5ba3af4
kgallagher <kevin.gallagher23@gmail.com>
2017-11-13 13:26:13 -0800

    map

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,13 +2,15 @@ class Location < ApplicationRecord
 	has_many :events
 
 	def to_json
-		"{'id': #{id}, 'longitude': #{longitude}, 'latitude': #{latitude}}"
+		"{'id': #{id}, 'longitude': #{longitude}, 'latitude': #{latitude}, 'name': #{name}, 'address': #{address}"
 	end
 	def to_h
 		{
 			id: id,
 			lng: longitude.to_f,
-			lat: latitude.to_f
+			lat: latitude.to_f,
+			name: name,
+			address: address
 		}
 	end
 
