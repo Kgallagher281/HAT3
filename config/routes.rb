2d55c6f749ba8f24a9ce29ce4f017a53b8cebf93
kgallagher <kevin.gallagher23@gmail.com>
2017-11-13 14:27:03 -0800

    sherill changes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :events
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'home#index'
 
 
 end
